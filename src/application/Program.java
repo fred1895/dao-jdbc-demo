b5d2594431d3236f50559a67bc9c439392be309c
Fred Almeida <fredericonascimento97@gmail.com>
2020-08-10 22:55:21 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Magazines");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Fred", "fred@gmail.com", new Date(), 1080.90, obj);
+		System.out.println(seller);
 
 	}
 
