0da54985e765cd4021a8788188db4fa2a4f18496
Fred Almeida <fredericonascimento97@gmail.com>
2020-08-11 10:57:45 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,15 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Magazines");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Fred", "fred@gmail.com", new Date(), 1080.90, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(7);
 		System.out.println(seller);
 
 	}
